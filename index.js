3822f9d1c059c906f36d05858e7b75365f067482
Indika Eranga <indikaeranga06@gmail.com>
2024-02-26 13:26:20 +0100

    45

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 // Create a server object
 http.createServer(function (req, res) {
-  res.write('Hello! Welcome to Nodejs simple web application start. start in a docker container'); // Write a response to the client
+  res.write('Hello! Welcome to Nodejs simple web application start. start in a docker container new one 45'); // Write a response to the client
   res.end(); // End the response
 }).listen(3000); // The server object listens on port 3000
